23ff37b410578f414a1af3c423595a1dce463069
Bibekbista12 <Bibekbista12@gmail.com>
2026-05-25 20:08:26 +0545

    Fix Render deployment issues

--- treknepal/settings.py
+++ treknepal/settings.py
@@ -5,7 +5,6 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = [
     "trekkk-nepal.onrender.com",
