cdb3365845c0c0c504654c3c4befb050553c4c11
Bibekbista12 <Bibekbista12@gmail.com>
2026-05-25 19:59:22 +0545

    Fix Render deployment issues

--- treknepal/settings.py
+++ treknepal/settings.py
@@ -104,3 +104,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
