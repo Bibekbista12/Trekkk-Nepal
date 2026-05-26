76c8549f366ad4f78f5fbbc823cc12ef4afc37f5
Bibekbista12 <Bibekbista12@gmail.com>
2026-05-26 17:25:45 +0545

    Add build.sh for render deployment

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
+
+
+
 #!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrated
+python manage.py migrate
+echo "from users.models import CustomUser; CustomUser.objects.filter(username='admin').exists() or CustomUser.objects.create_superuser('admin', 'admin@treknepal.com', 'Admin@123', role='agency')" | python manage.py shell
