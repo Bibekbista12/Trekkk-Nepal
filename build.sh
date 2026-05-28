6f6fd5dfd703d530cba93bf9378b4d7b71ef9163
Bibekbista12 <Bibekbista12@gmail.com>
2026-05-28 15:36:36 +0545

    Load trek images on deploy

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
-
-
-
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-echo "from users.models import CustomUser; CustomUser.objects.filter(username='admin').exists() or CustomUser.objects.create_superuser('admin', 'admin@treknepal.com', 'Admin@123', role='agency')" | python manage.py shell
+python manage.py loaddata trekimages.json
+echo "
+from users.models import CustomUser
+if not CustomUser.objects.filter(username='admin').exists():
+    CustomUser.objects.create_superuser('admin', 'admin@treknepal.com', 'Admin@123', role='agency')
+    print('Admin created')
+else:
+    u = CustomUser.objects.get(username='admin')
+    u.set_password('Admin@123')
+    u.role = 'agency'
+    u.is_staff = True
+    u.is_superuser = True
+    u.save()
+    print('Admin updated')
+" | python manage.py shell
